ffb3483085005c947b57aae101026b17b79bfe1a
Dimitar Zahariev <dimitar@zahariev.pro>
2017-10-25 16:06:44 +0300

    Added UTF8

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -1,5 +1,5 @@
-create database bulgaria;
-grant all on bulgaria.* to web_user identified by 'Password1';
+create database bulgaria character set utf8 collate utf8_general_ci;
+grant all on bulgaria.* to 'web_user'@'%' identified by 'Password1';
 use bulgaria;
 create table cities (id int primary key auto_increment, city_name varchar(50), population int);
 INSERT INTO cities (city_name, population) VALUES ('София', 1236047);
